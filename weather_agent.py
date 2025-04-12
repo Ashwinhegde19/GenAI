c6f757611f0fd815e7933c1adc587773c31a1e21
Ashwinhegde19 <ashwinhegde19@gmail.com>
2025-04-13 02:16:59 +0530

    add agent pushing

--- weather_agent.py
+++ weather_agent.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+import requests
 import json
 import os
 
@@ -8,7 +9,29 @@ load_dotenv()
 client = OpenAI()
 
 def get_weather(city: str):
-    return f"The weather in {city} is 28 degree C."
+    print("🔨 Tool Called: get_weather", city)
+    
+    url = f"https://wttr.in/{city}?format=%C+%t"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return f"The weather in {city} is {response.text}."
+    return "Something went wrong"
+
+def run_command(command):
+    result = os.system(command=command)
+    return result
+
+available_tools = {
+    "get_weather": {
+        "fn": get_weather,
+        "description": "Takes a city name as an input and returns the current weather for the city"
+    },
+    "run_command": {
+        "fn": run_command,
+        "description": "Takes a command as input to execute on system and returns ouput"
+    }
+}
 
 system_prompt = """
     You are an helpful AI Assistant who is specialized in resolving user query.
@@ -30,6 +53,10 @@ system_prompt = """
         "input": "The input parameter for the function",
     }}
 
+    Available Tools:
+    - get_weather: Takes a city name as an input and returns the current weather for the city
+    - run_command: Takes a command as input to execute on system and returns output
+
 
     Example:
     User Query: What is the weather of new york?
@@ -41,18 +68,38 @@ system_prompt = """
 
 """
 
-completion = client.chat.completions.create(
-  model="gpt-4o",
-  response_format={"type": "json_object"},
-  messages=[
-      {"role": "system", "content": system_prompt},
-      {"role": "user", "content": "what is the weather of Bengaluru"},
-      {"role": "user", "content": json.dumps({"step": "plan", "content": "The user is interested in weather data of Bengaluru"})},
-      {"role": "user", "content": json.dumps({"step": "plan", "content": "From the available tools I should call get_weather"})},
-      {"role": "user", "content": json.dumps({"step": "action", "function": "get_weather", "input": "Bengaluru"})},
-      {"role": "user", "content": json.dumps({"step": "observe", "output": get_weather("Bengaluru")})},
-      {"role": "user", "content": json.dumps({"step": "output", "content": "The weather for Bengaluru seems to be 28 degrees."})}
-  ]
-)
+messages = [
+    { "role": "system", "content": system_prompt }
+]
 
-print(completion.choices[0].message.content)
+while True:
+    user_query = input('> ')
+    messages.append({ "role": "user", "content": user_query })
+
+    while True:
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            response_format={"type": "json_object"},
+            messages=messages
+        )
+
+        parsed_output = json.loads(response.choices[0].message.content)
+        messages.append({ "role": "assistant", "content": json.dumps(parsed_output) })
+
+        if parsed_output.get("step") == "plan":
+            print(f"🧠: {parsed_output.get("content")}")
+            continue
+        
+        if parsed_output.get("step") == "action":
+            tool_name = parsed_output.get("function")
+            tool_input = parsed_output.get("input")
+
+            if available_tools.get(tool_name, False) != False:
+                output = available_tools[tool_name].get("fn")(tool_input)
+                messages.append({ "role": "assistant", "content": json.dumps({ "step": "observe", "output":  output}) })
+                continue
+        
+        if parsed_output.get("step") == "output":
+            print(f"🤖: {parsed_output.get("content")}")
+            break
+
